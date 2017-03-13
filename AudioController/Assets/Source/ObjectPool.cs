320a6c1fbc48f8633fd3b5672446173289118e0b
dimixar <dimasalmaz@gmail.com>
2017-03-13 20:59:08 +0200

    Create ObjectPool.

---
+++ AudioController/Assets/Source/ObjectPool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefab;
+
+    private Stack<GameObject> _pool;
+
+    public GameObject GetGameObject()
+    {
+        if (_pool != null)
+        {
+            if (_pool.Count > 0)
+                return _pool.Pop();
+            else
+                return GameObject.Instantiate<GameObject>(_prefab);
+        }
+        else
+        {
+            _pool = new Stack<GameObject>();
+            return GameObject.Instantiate<GameObject>(_prefab);
+        }
+    }
+
+    public void Put(GameObject obj)
+    {
+        if (_pool == null)
+            _pool = new Stack<GameObject>();
+        
+        _pool.Push(obj);
+    }
+}
