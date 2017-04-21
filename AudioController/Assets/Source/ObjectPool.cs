3bd05d11dececbf9b8fd9a11d00d0711e56fabbb
Moloșnic Dumitru <dimasalmaz@gmail.com>
2017-04-21 13:21:49 +0300

    Implement Prefab based pool.

--- AudioController/Assets/Source/ObjectPool.cs
+++ AudioController/Assets/Source/ObjectPool.cs
@@ -4,20 +4,61 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    #region Public fields
+    public List<PrefabBasedPool> pools;
+    #endregion
     #region Public methods and properties
 
-    //TODO: Add possibility to differentiate between prefabs
     public AudioObject GetFreeAudioObject(GameObject prefab = null)
     {
+        if (prefab == null)
+            return null;
+
         return null;
     }
 
+    #endregion
+
+    #region Monobehaviour methods
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        pools = new List<PrefabBasedPool>();
+    }
     #endregion
 }
 
 [System.Serializable]
 public class PrefabBasedPool
 {
+    public PrefabBasedPool(GameObject prefab)
+    {
+        pool = new List<GameObject>();
+        this.prefab = prefab;
+    }
     public GameObject prefab;
-    public List<AudioObject> audioObjects;
+    public List<GameObject> pool;
+
+    /// <summary>
+    /// Where pooled objects will reside.
+    /// </summary>
+    public Transform parent;
+
+    public GameObject GetFreeObject()
+    {
+        GameObject freeObj = pool.Find((x) => {
+            var poolable = x.GetComponent<IPoolable>();
+            return poolable.IsFree();
+        });
+
+        if (freeObj != null)
+            return freeObj;
+
+        var obj = GameObject.Instantiate(prefab, parent);
+        pool.Add(obj);
+
+        return obj;
+    }
 }
