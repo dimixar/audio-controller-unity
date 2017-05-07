624404f9a8d7d53f9a929b77bda33668dd944d46
Molosnic Dumitru <dimasalmaz@gmail.com>
2017-05-07 20:40:14 +0300

    Add Some editor controls over the audio object.

---
+++ AudioController/Assets/Source/Editor/AudioObjectEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using OSAC;
+
+namespace OSAC.Editor
+{
+    [CustomEditor(typeof(AudioObject))]
+    public class AudioObjectEditor : UnityEditor.Editor
+    {
+        private AudioObject _ao;
+
+        public override void OnInspectorGUI()
+        {
+            _ao = target as AudioObject;
+
+            EditorGUILayout.LabelField("Sound Item", _ao.ID, EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Current Clip", _ao.clipName);
+            if (_ao.source.isPlaying)
+            {
+                if (GUILayout.Button("Stop"))
+                {
+                    _ao.Stop();
+                }
+            }
+        }
+    }
+}
