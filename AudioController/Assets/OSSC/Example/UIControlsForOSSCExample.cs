6830d4573ef579f1a401d787dc86aa19f165f3ca
Molosnic Dumitru <dimasalmaz@gmail.com>
2017-08-26 20:20:10 +0300

    Finish the example scene.

--- AudioController/Assets/OSSC/Example/UIControlsForOSSCExample.cs
+++ AudioController/Assets/OSSC/Example/UIControlsForOSSCExample.cs
@@ -1,16 +1,138 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OSSC;
 
-public class UIControlsForOSSCExample : MonoBehaviour {
+public class UIControlsForOSSCExample : MonoBehaviour
+{
+    public SoundController soundController;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    SoundCueProxy coins;
+    SoundCueProxy whosh;
+    SoundCueProxy crackingIce;
+    SoundCueProxy sfxCue;
+    SoundCueProxy sfxCueLoop;
+    SoundCueProxy music;
+
+    public void PlayCoins()
+    {
+        //You should send this struct to soundController in order to play something.
+        PlaySoundSettings settings = new PlaySoundSettings();
+        settings.Init();
+
+        settings.name = "Coins";
+        //SoundController returns a SoundCueProxy, that can be reused or subscribe to one of it's events.
+        coins = soundController.Play(settings);
+    }
+
+    public void PlayWhosh()
+    {
+        PlaySoundSettings settings = new PlaySoundSettings();
+        settings.Init();
+
+        settings.name = "Whosh";
+        whosh = soundController.Play(settings);
+        whosh.OnPlayCueEnded += (Cue) =>
+        {
+            settings = new PlaySoundSettings();
+            settings.Init();
+            //It is a good practice to reuse the cue you already played.
+            //Because soundCueProxy caches all the data and SoundController
+            //will not have to go through finding it again.
+            settings.soundCueProxy = whosh;
+            soundController.Play(settings);
+        };
+    }
+
+    public void PlayCrackingIce()
+    {
+        PlaySoundSettings settings = new PlaySoundSettings();
+        settings.Init();
+
+        settings.name = "CrackingIce";
+        crackingIce = soundController.Play(settings);
+    }
+
+    public void PlaySFXCue()
+    {
+        PlaySoundSettings settings = new PlaySoundSettings();
+        settings.Init();
+
+        settings.names = new[] {"Coins", "Whosh", "CrackingIce"};
+        sfxCue = soundController.Play(settings);
+    }
+
+    public void PlaySFXCueLooped()
+    {
+        PlaySoundSettings settings = new PlaySoundSettings();
+        settings.Init();
+
+        settings.names = new[] {"Coins", "Whosh", "CrackingIce"};
+        settings.isLooped = true;
+        sfxCueLoop = soundController.Play(settings);
+
+        //You can also check when any cue finished playing
+        sfxCueLoop.OnPlayCueEnded += (cue) =>
+        {
+            Debug.LogFormat("Cue with ID {0} finished playing", cue.ID);
+        };
+
+        sfxCueLoop.OnPlayEnded += (name) => Debug.LogFormat("Sound \"{0}\" finished playing", name);
+    }
+
+    public void PlayMusic()
+    {
+        PlaySoundSettings settings = new PlaySoundSettings();
+        settings.Init();
+
+        settings.name = "Ballade";
+        settings.fadeInTime = 1f;
+        settings.fadeOutTime = 2f;
+        settings.categoryName = "Music"; // search only in that category
+        settings.parent = transform; // Use this parent to put the AudioSource's position here.
+        settings.isLooped = true;
+        music = soundController.Play(settings);
+    }
+
+    public void StopCoins()
+    {
+        if (coins != null)
+            coins.Stop();
+    }
+
+    public void StopWhosh()
+    {
+        if (whosh != null)
+            whosh.Stop();
+    }
+
+    public void StopCrackingIce()
+    {
+        if (crackingIce != null)
+            crackingIce.Stop();
+    }
+
+    public void StopSFXCue()
+    {
+        if (sfxCue != null)
+            sfxCue.Stop();
+    }
+
+    public void StopSFXCueLooped()
+    {
+        if (sfxCueLoop != null)
+            sfxCueLoop.Stop();
+    }
+
+    public void StopMusic()
+    {
+        if (music != null)
+            music.Stop();
+    }
+
+    public void StopAll()
+    {
+        //Stop all playing cues by calling this method;
+        soundController.StopAll();
+    }
 }
